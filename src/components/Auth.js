c75bd0dc6b7867c041bf9f58f6e5f4cb6ccaa327
Rafael Santana <80044042+rafaelrsantana@users.noreply.github.com>
2024-07-23 22:45:21 -0300

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -9,7 +9,27 @@ const AuthContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 50px;
+  justify-content: center;
+  height: 100vh;
+  background: url('https://i.pinimg.com/originals/6c/9c/79/6c9c79fbdca68a73e0ac5a0b9b828733.jpg') no-repeat center center fixed; 
+  background-size: cover;
+  background-color: pink;
+`;
+
+const AuthBox = styled.div`
+  background-color: rgba(255, 255, 255, 0.8); /* Cor de fundo com transparência */
+  border-radius: 15px;
+  padding: 40px 30px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  max-width: 400px;
+  width: 100%;
+`;
+
+const Title = styled.h2`
+  margin-bottom: 20px;
+  font-size: 2em;
+  color: #333;
 `;
 
 const AuthInput = styled.input`
@@ -17,6 +37,7 @@ const AuthInput = styled.input`
   padding: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
+  width: calc(100% - 22px); /* Para evitar overflow */
 `;
 
 const AuthButton = styled.button`
@@ -27,6 +48,12 @@ const AuthButton = styled.button`
   border: none;
   border-radius: 5px;
   cursor: pointer;
+  width: 100%;
+  font-size: 1em;
+
+  &:hover {
+    background-color: #0056b3;
+  }
 `;
 
 const Auth = () => {
@@ -56,20 +83,23 @@ const Auth = () => {
 
   return (
     <AuthContainer>
-      <AuthInput 
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <AuthInput 
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <AuthButton onClick={handleSignUp}>Sign Up</AuthButton>
-      <AuthButton onClick={handleSignIn}>Sign In</AuthButton>
+      <AuthBox>
+        <Title>Bem vindo(a)!</Title>
+        <AuthInput 
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <AuthInput 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <AuthButton onClick={handleSignUp}>Sign Up</AuthButton>
+        <AuthButton onClick={handleSignIn}>Sign In</AuthButton>
+      </AuthBox>
     </AuthContainer>
   );
 };
